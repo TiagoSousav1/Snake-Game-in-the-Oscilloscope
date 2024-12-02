03044be9829665051b37da262cc93d629eca11ff
Tiago Sousa laptop <up201907205@fe.up.pt>
2024-12-02 16:42:51 +0000

    added funcitons to draw snake

--- Oscilloscope Snake game/src/main.cpp
+++ Oscilloscope Snake game/src/main.cpp
@@ -1,31 +1,60 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-// MCP4822 configuration
-const int CS_PIN = 10;    // Chip select pin for MCP4822
-
-const int centerX = 2048;    // Center X position (mid-range of DAC)
-const int centerY = 2048;    // Center Y position (mid-range of DAC)
-const int sideLength = 4095; // Side length of the square
-
-SPISettings spiSets (20000000, MSBFIRST, SPI_MODE0);
-
 // MCP4822 DAC control bits
 #define MCP4822_CHANNEL_1 0x3000
 #define MCP4822_CHANNEL_2 0xB000
+#define CS_PIN 10
+
 #define RIGHT_BUTTON_PIN 7
 #define LEFT_BUTTON_PIN 6
 #define UP_BUTTON_PIN 5
 #define DOWN_BUTTON_PIN 4
+#define INTERRUPT_PIN 2
 
 #define RIGHT 0
 #define LEFT 1
 #define UP 2
 #define DOWN 3
 
-static int directionState = UP;
+#define BOARD_SIZE 256
 
+const int stepSize = 16;
 
+int snake[256][2];
+int snakeLength = 40;
+
+SPISettings spiSets (20000000, MSBFIRST, SPI_MODE0);
+
+int directionState = UP;
+
+void inputInterrupt()
+{
+  Serial.println("I WAS INTERRUPTED");
+  if(digitalRead(RIGHT_BUTTON_PIN) == LOW)
+  {
+    directionState = RIGHT;
+    digitalWrite(LED_BUILTIN, HIGH); 
+  }
+  if(digitalRead(LEFT_BUTTON_PIN) == LOW)
+  {
+    directionState = LEFT;
+    digitalWrite(LED_BUILTIN, HIGH); 
+  }
+    
+  if(digitalRead(UP_BUTTON_PIN) == LOW)
+  {
+    directionState = UP;
+    digitalWrite(LED_BUILTIN, HIGH); 
+  }
+    
+  if(digitalRead(DOWN_BUTTON_PIN) == LOW)
+  {
+    directionState = DOWN;
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+    
+}
 
 // Function to send data to the MCP4822 DAC
 void writeDAC(uint16_t channel, uint16_t value) {
@@ -36,17 +65,79 @@ void writeDAC(uint16_t channel, uint16_t value) {
   SPI.endTransaction();
 }
 
+// DRAW SNAKE
+void initializeSnake()
+{
+  int j = 100;
+  for(int i = 0; i < snakeLength; i++)
+  { 
+    snake[i][0] = 100;
+    snake[i][1] = j;
+    j++;
+  }  
+}
 
+void drawPoint(int x, int y)
+{
+  writeDAC(MCP4822_CHANNEL_1, x);
+  writeDAC(MCP4822_CHANNEL_2, y);
+}
+
+void drawSnake()
+{
+  for (int i = 0; i < snakeLength; i++)
+  {
+    drawPoint(snake[i][0], snake[i][1]);
+  }
+
+}
+
+void nextSnake()
+{
+  // Shift all elements back by one
+  for (int i = 1; i < snakeLength; i++)
+  {
+      snake[i - 1][0] = snake[i][0];
+      snake[i - 1][1] = snake[i][1];
+  }
+
+  if(directionState == UP)
+  {
+    snake[0][0] = snake[1][0];
+    snake[0][1] = snake[1][1]+1;
+    return;
+  }
+  if(directionState == DOWN)
+  {
+    snake[0][0] = snake[1][0];
+    snake[0][1] = snake[1][1]-1;
+    return;
+  }
+  if(directionState == RIGHT)
+  {
+    snake[0][0] = snake[1][0]+1;
+    snake[0][1] = snake[1][1];
+    return;
+  }
+  if(directionState == LEFT)
+  {
+    snake[0][0] = snake[1][0]-1;
+    snake[0][1] = snake[1][1];
+    return;
+  }
+
+
+}
+
+// DRAWING BOARD
 void drawVerticalLine(int x, int y_start, int y_end)
 {
   writeDAC(MCP4822_CHANNEL_2, x);
   for(int i = y_start; i < y_end; i++)
   {
-
     writeDAC(MCP4822_CHANNEL_1, i);
-    //delay(0.1);
+    delay(0.1);
   }
-
 }
 
 void drawHorizontalLine(int y, int x_start, int x_end)
@@ -54,96 +145,22 @@ void drawHorizontalLine(int y, int x_start, int x_end)
   writeDAC(MCP4822_CHANNEL_1, y);
   for(int i = x_start; i < x_end; i++)
   {
-    
     writeDAC(MCP4822_CHANNEL_2, i);
-    //delay(0.1);
+    delay(0.1);
   }
-
 }
 
-// Function to draw a square
+// Function to draw the boundary square
 void drawSquare() {
-  // Define square corners
-  int x1 = centerX - sideLength / 2;
-  int y1 = centerY - sideLength / 2;
-  
-  int x2 = centerX + sideLength / 2;
-  int y2 = y1;
-  
-  int x3 = x2;
-  int y3 = centerY + sideLength / 2;
-  
-  int x4 = x1;
-  int y4 = y3;
-
-  // Draw the square by moving between corners
-  // Start at (x1, y1) -> (x2, y2) -> (x3, y3) -> (x4, y4) -> back to (x1, y1)
-  
-  // Move from (x1, y1) to (x2, y2)
-  writeDAC(MCP4822_CHANNEL_1, x1);
-  writeDAC(MCP4822_CHANNEL_2, y1);
-  //delay(10);
-  
-  writeDAC(MCP4822_CHANNEL_1, x2);
-  writeDAC(MCP4822_CHANNEL_2, y2);
-  //delay(10);
-
-  // Move from (x2, y2) to (x3, y3)
-  writeDAC(MCP4822_CHANNEL_1, x2);
-  writeDAC(MCP4822_CHANNEL_2, y2);
-  //delay(10);
-  
-  writeDAC(MCP4822_CHANNEL_1, x3);
-  writeDAC(MCP4822_CHANNEL_2, y3);
-  //delay(10);
-
-  // Move from (x3, y3) to (x4, y4)
-  writeDAC(MCP4822_CHANNEL_1, x3);
-  writeDAC(MCP4822_CHANNEL_2, y3);
-  //delay(10);
-  
-  writeDAC(MCP4822_CHANNEL_1, x4);
-  writeDAC(MCP4822_CHANNEL_2, y4);
-  //delay(10);
-
-  // Move from (x4, y4) back to (x1, y1)
-  writeDAC(MCP4822_CHANNEL_1, x4);
-  writeDAC(MCP4822_CHANNEL_2, y4);
-  //delay(10);
-  
-  writeDAC(MCP4822_CHANNEL_1, x1);
-  writeDAC(MCP4822_CHANNEL_2, y1);
-  //delay(10);
+  drawVerticalLine(0, 0, BOARD_SIZE); // Left side
+  drawHorizontalLine(BOARD_SIZE, 0, BOARD_SIZE); // Top side
+  drawVerticalLine(BOARD_SIZE, 0, BOARD_SIZE);  // Right side
+  drawHorizontalLine(0, 0, BOARD_SIZE); // Bot side
 }
 
-void updateDirectionStateRight()
-{
-  directionState = RIGHT;
-  Serial.print(directionState);
-}
-
-void updateDirectionStateLeft()
-{
-  directionState = LEFT;
-  Serial.print(directionState);
-}
-
-void updateDirectionStateUp()
-{
-  directionState = UP;
-  Serial.print(directionState);
-}
-
-void updateDirectionStateDown()
-{
-  directionState = DOWN;
-  Serial.print(directionState);
-}
 
 void setup() {
-  // DEBUGGING
   Serial.begin(9600);
-
   // SPI settings
   SPI.begin();
   //SPI.setClockDivider(SPI_CLOCK_DIV2); // Set SPI speed
@@ -151,30 +168,37 @@ void setup() {
   pinMode(CS_PIN, OUTPUT);
   digitalWrite(CS_PIN, HIGH);  // Ensure CS pin is high
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(RIGHT_BUTTON_PIN, INPUT);
+  pinMode(LEFT_BUTTON_PIN, INPUT);
+  pinMode(UP_BUTTON_PIN, INPUT);
+  pinMode(DOWN_BUTTON_PIN, INPUT);
+
 
   // BUTTONS
-  pinMode(RIGHT_BUTTON_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(RIGHT_BUTTON_PIN), updateDirectionStateRight, FALLING);
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), inputInterrupt, FALLING);
 
-  pinMode(LEFT_BUTTON_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(LEFT_BUTTON_PIN), updateDirectionStateLeft, FALLING);
+  //SNAKE
+  initializeSnake();
 
-  pinMode(UP_BUTTON_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(UP_BUTTON_PIN), updateDirectionStateUp, FALLING);
-
-  pinMode(DOWN_BUTTON_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(DOWN_BUTTON_PIN), updateDirectionStateDown, FALLING);
 }
 
 void loop() {
-  // ponto a ponto - bastante ruído
-  //drawVerticalLine(0, 0, 2048); // Left side
-  //drawHorizontalLine(2048, 0, 4095); // Top side
-  //drawVerticalLine(4095, 0, 2048);  // Right side
-  //drawHorizontalLine(0, 0, 4095); // Bot side
+  for(int i=0; i < snakeLength; i++)
+  {
+    Serial.println(snake[i][0]);
+    Serial.println(snake[i][1]);
+  }
 
-  //drawLine(1024, 4096);
+
 
   drawSquare();
+  drawSnake();
+  nextSnake();
+
+  delay(1000);
+
 }
+
 
