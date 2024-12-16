c6658fde0efc82ba6ded565d4c69d9b86110cc20
Tiago Sousa laptop <up201907205@fe.up.pt>
2024-12-16 17:22:25 +0000

    Added sync outputs to fix bug were extra fruit was being drawned, added kernel.

--- Oscilloscope Snake game/src/main.cpp
+++ Oscilloscope Snake game/src/main.cpp
@@ -3,17 +3,19 @@
 #include <time.h>
 #include <stdlib.h>
 
-// MCP4822 DAC control bits
-#define MCP4822_CHANNEL_1 0x3000
-#define MCP4822_CHANNEL_2 0xB000
+
 #define CS_PIN 10
+#define LDAC 8
+#define MCP4822_CHANNEL_1  0x3000  // Select channel A (bit 14)
+#define MCP4822_CHANNEL_2  0xB000  // Select channel B (bit 14)
+
 
 #define RIGHT_BUTTON_PIN 7
 #define LEFT_BUTTON_PIN 6
 #define UP_BUTTON_PIN 5
 #define DOWN_BUTTON_PIN 4
 #define INTERRUPT_PIN 2
-#define TIME_PIN 8
+#define TIME_PIN 9
 
 #define RIGHT 0
 #define LEFT 1
@@ -22,6 +24,12 @@
 
 #define BOARD_SIZE 256
 #define EATING_RADIUS 10
+#define SNAKE_STEP 2
+
+#define CLOCK_FREQ 2000
+#define FRAMES_PER_SECOND 60
+#define NT 4
+int cur_task = NT;
 
 uint16_t snake[BOARD_SIZE][2];
 uint16_t fruit[2] = {150, 150};
@@ -32,6 +40,95 @@ SPISettings spiSets (20000000, MSBFIRST, SPI_MODE0);
 int directionState = UP;
 
 
+typedef struct {
+  /* period in ticks */
+  int period;
+  /* ticks until next activation */
+  int delay;
+  /* function pointer */
+  void (*func)(void);
+  /* activation counter */
+  int exec;
+} Sched_Task_t;
+
+Sched_Task_t Tasks[NT];
+
+void Sched_Init(void)
+{
+  for(int x=0; x<NT; x++)
+    Tasks[x].func = 0;
+
+  /* Also configures interrupt that periodically calls Sched_Schedule(). */
+  noInterrupts(); // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+ 
+  //OCR1A = 6250; // compare match register 16MHz/256/10Hz
+  //OCR1A = 31250; // compare match register 16MHz/256/2Hz
+  OCR1A = 31;    // compare match register 16MHz/256/2kHz
+  //OCR1A = 1563;    // compare match register 16MHz/256/40Hz
+  TCCR1B |= (1 << WGM12); // CTC mode
+  TCCR1B |= (1 << CS12); // 256 prescaler
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  interrupts(); // enable all interrupts  
+}
+
+int Sched_AddT(void (*f)(void), int d, int p){
+  for(int x=0; x<NT; x++)
+  {
+    if (!Tasks[x].func)
+    {
+      Tasks[x].period = p;
+      Tasks[x].delay = d;
+      Tasks[x].exec = 0;
+      Tasks[x].func = f;
+      return x;
+    }
+  }
+
+  return -1;
+}
+
+void Sched_Schedule(void)
+{
+  for(int x=0; x<NT; x++) {
+    if(Tasks[x].func)
+    {
+      if(Tasks[x].delay)
+      {
+        Tasks[x].delay--;
+      } 
+      else
+      {
+        /* Schedule Task */
+        Tasks[x].exec++;
+        Tasks[x].delay = Tasks[x].period-1;
+      }
+    }
+  }
+}
+
+void Sched_Dispatch(void){
+  int prev_task = cur_task;
+  for(int x=0; x<cur_task; x++)
+  {
+    if((Tasks[x].func)&&(Tasks[x].exec))
+    {
+      Tasks[x].exec=0;
+      cur_task = x;
+      interrupts();
+      Tasks[x].func();
+      noInterrupts();
+      cur_task = prev_task;
+      /* Delete task if one-shot */
+      if(!Tasks[x].period) Tasks[x].func = 0;
+    }
+  }
+}
+
+
+// Buttons
 void inputInterrupt()
 {
   //Serial.println("I WAS INTERRUPTED");
@@ -60,17 +157,68 @@ void inputInterrupt()
     
 }
 
-// Function to send data to the MCP4822 DAC
-void writeDAC(uint16_t channel, uint16_t value) {
-  value = value << 3;
+void drawPoint(uint16_t x, uint16_t y)
+{
+
+  x = x << 3;
+  y = y << 3;
+
   SPI.beginTransaction(spiSets);
   digitalWrite(CS_PIN, LOW);
-  SPI.transfer16(channel | (value & 0x0FFF));  // Send channel + value
+  SPI.transfer16(MCP4822_CHANNEL_1 | (x & 0x0FFF));
+  digitalWrite(CS_PIN, HIGH);
+
+  digitalWrite(CS_PIN, LOW);
+  SPI.transfer16(MCP4822_CHANNEL_2 | (y & 0x0FFF));
   digitalWrite(CS_PIN, HIGH);
   SPI.endTransaction();
+
+  // Apply changes simultaneously by pulsing LDAC low
+  digitalWrite(LDAC, LOW);
+  delayMicroseconds(1);  // Ensure LDAC pulse is at least 1 microsecond
+  digitalWrite(LDAC, HIGH);
+    
+
 }
 
-// DRAW SNAKE
+void drawSnake()
+{
+  for (int i = 0; i < snakeLength; i++)
+  {
+    drawPoint(snake[i][0], snake[i][1]);
+  }
+
+}
+
+// DRAWING BOARD
+void drawVerticalLine(int x, int y_start, int y_end)
+{
+  for(int i = y_start; i < y_end; i+=3)
+  {
+    drawPoint(x, i);
+  }
+}
+
+void drawHorizontalLine(int y, int x_start, int x_end)
+{
+  for(int i = x_start; i < x_end; i+=3)
+  {
+    drawPoint(y, i);
+  }
+}
+
+// Function to draw the boundary square
+// Function to draw the boundary square
+void drawSquare() 
+{
+  drawVerticalLine(0, 0, BOARD_SIZE); // Left side
+  drawHorizontalLine(BOARD_SIZE, 0, BOARD_SIZE); // Top side
+  drawVerticalLine(BOARD_SIZE, 0, BOARD_SIZE);  // Right side
+  drawHorizontalLine(0, 0, BOARD_SIZE); // Bot side
+}
+
+
+// Snake Logic
 void initializeSnake()
 {
   int j = 200;
@@ -82,19 +230,9 @@ void initializeSnake()
   }  
 }
 
-void drawPoint(int x, int y)
+void drawFruit()
 {
-  writeDAC(MCP4822_CHANNEL_1, x);
-  writeDAC(MCP4822_CHANNEL_2, y);
-}
-
-void drawSnake()
-{
-  for (int i = 0; i < snakeLength; i++)
-  {
-    drawPoint(snake[i][0], snake[i][1]);
-  }
-
+  drawPoint(fruit[0], fruit[1]);
 }
 
 void nextSnake()
@@ -110,7 +248,7 @@ void nextSnake()
   if(directionState == UP)
   {
     snake[0][0] = snake[1][0];
-    snake[0][1] = snake[1][1]+1;
+    snake[0][1] = snake[1][1]+SNAKE_STEP;
 
     if(((snake[0][0] >= fruit[0] - EATING_RADIUS && snake[0][0] <= fruit[0] + EATING_RADIUS)) && (snake[0][1] >= fruit[1] - EATING_RADIUS && snake[0][1] <= fruit[1] + EATING_RADIUS))
     {
@@ -131,7 +269,7 @@ void nextSnake()
   else if(directionState == DOWN)
   {
     snake[0][0] = snake[1][0];
-    snake[0][1] = snake[1][1]-1;
+    snake[0][1] = snake[1][1]-SNAKE_STEP;
 
     if(((snake[0][0] >= fruit[0] - EATING_RADIUS && snake[0][0] <= fruit[0] + EATING_RADIUS)) && (snake[0][1] >= fruit[1] - EATING_RADIUS && snake[0][1] <= fruit[1] + EATING_RADIUS))
     {
@@ -150,7 +288,7 @@ void nextSnake()
 
   else if(directionState == RIGHT)
   {
-    snake[0][0] = snake[1][0]+1;
+    snake[0][0] = snake[1][0]+SNAKE_STEP;
     snake[0][1] = snake[1][1];
 
 
@@ -169,7 +307,7 @@ void nextSnake()
 
   else if(directionState == LEFT)
   {
-    snake[0][0] = snake[1][0]-1;
+    snake[0][0] = snake[1][0]-SNAKE_STEP;
     snake[0][1] = snake[1][1];
 
     
@@ -188,34 +326,6 @@ void nextSnake()
   }
 }
 
-// DRAWING BOARD
-void drawVerticalLine(int x, int y_start, int y_end)
-{
-  writeDAC(MCP4822_CHANNEL_2, x);
-  for(int i = y_start; i < y_end; i+=3)
-  {
-    writeDAC(MCP4822_CHANNEL_1, i);
-  }
-}
-
-void drawHorizontalLine(int y, int x_start, int x_end)
-{
-  writeDAC(MCP4822_CHANNEL_1, y);
-  for(int i = x_start; i < x_end; i+=3)
-  {
-    writeDAC(MCP4822_CHANNEL_2, i);
-  }
-}
-
-// Function to draw the boundary square
-// Function to draw the boundary square
-void drawSquare() {
-  drawVerticalLine(0, 0, BOARD_SIZE); // Left side
-  drawHorizontalLine(BOARD_SIZE, 0, BOARD_SIZE); // Top side
-  drawVerticalLine(BOARD_SIZE, 0, BOARD_SIZE);  // Right side
-  drawHorizontalLine(0, 0, BOARD_SIZE); // Bot side
-}
-
 
 void setup() {
   // To create random numbers
@@ -224,9 +334,12 @@ void setup() {
   // SPI settings
   SPI.begin();
   //SPI.setClockDivider(SPI_CLOCK_DIV2); // Set SPI speed
+  pinMode(LDAC, OUTPUT);
+  digitalWrite(LDAC, HIGH);
 
-  pinMode(CS_PIN, OUTPUT);
+  pinMode(CS_PIN, OUTPUT);  
   digitalWrite(CS_PIN, HIGH);  // Ensure CS pin is high
+
 
   pinMode(RIGHT_BUTTON_PIN, INPUT);
   pinMode(LEFT_BUTTON_PIN, INPUT);
@@ -239,24 +352,27 @@ void setup() {
   pinMode(INTERRUPT_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), inputInterrupt, FALLING);
 
-  //SNAKE
+  // SNAKE
   initializeSnake();
 
+  // Tasks
+  Sched_Init();
+  // Task, delay, period
+  Sched_AddT(nextSnake, 0, 50);
+  Sched_AddT(drawFruit, 0,  50);
+  Sched_AddT(drawSnake, 0,  50);
+  Sched_AddT(drawSquare, 0,  50);
 }
 
-void loop() {
+ISR(TIMER1_COMPA_vect){//timer1 interrupt
+  Sched_Schedule();
+  Sched_Dispatch();
+}
 
-  digitalWrite(TIME_PIN, HIGH);
-  drawSquare();
-  digitalWrite(TIME_PIN, LOW);
-  drawSnake();
-  pinMode(TIME_PIN, HIGH);
-  nextSnake();
-  pinMode(TIME_PIN, LOW);
-
-  drawPoint(fruit[0], fruit[1]);
-  pinMode(TIME_PIN, HIGH);
+void loop()
+{
 
 }
+
 
 
