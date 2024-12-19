5fd174798ffe20a104bc9ae3f7ba89c6c1aa2407
hugofc14 <hugo.ff.cardoso@gmail.com>
2024-12-19 18:54:58 +0000

    beggining game over text funcs

--- Oscilloscope Snake game/src/main.cpp
+++ Oscilloscope Snake game/src/main.cpp
@@ -35,24 +35,24 @@ int directionState = UP;
 void inputInterrupt()
 {
   //Serial.println("I WAS INTERRUPTED");
-  if(digitalRead(RIGHT_BUTTON_PIN) == LOW)
+  if(digitalRead(RIGHT_BUTTON_PIN) == LOW && directionState != LEFT)
   {
     directionState = RIGHT;
     digitalWrite(LED_BUILTIN, HIGH); 
   }
-  if(digitalRead(LEFT_BUTTON_PIN) == LOW)
+  if(digitalRead(LEFT_BUTTON_PIN) == LOW && directionState != RIGHT)
   {
     directionState = LEFT;
     digitalWrite(LED_BUILTIN, HIGH); 
   }
     
-  if(digitalRead(UP_BUTTON_PIN) == LOW)
+  if(digitalRead(UP_BUTTON_PIN) == LOW && directionState != DOWN)
   {
     directionState = UP;
     digitalWrite(LED_BUILTIN, HIGH); 
   }
     
-  if(digitalRead(DOWN_BUTTON_PIN) == LOW)
+  if(digitalRead(DOWN_BUTTON_PIN) == LOW && directionState != UP)
   {
     directionState = DOWN;
     digitalWrite(LED_BUILTIN, HIGH);
@@ -88,106 +88,6 @@ void drawPoint(int x, int y)
   writeDAC(MCP4822_CHANNEL_2, y);
 }
 
-void drawSnake()
-{
-  for (int i = 0; i < snakeLength; i++)
-  {
-    drawPoint(snake[i][0], snake[i][1]);
-  }
-
-}
-
-void nextSnake()
-{
-  // Shift all elements back by one
-  for (int i = snakeLength; i > 0; i--)
-  {
-      snake[i][0] = snake[i - 1][0];
-      snake[i][1] = snake[i - 1][1];
-  }
-
-
-  if(directionState == UP)
-  {
-    snake[0][0] = snake[1][0];
-    snake[0][1] = snake[1][1]+1;
-
-    if(((snake[0][0] >= fruit[0] - EATING_RADIUS && snake[0][0] <= fruit[0] + EATING_RADIUS)) && (snake[0][1] >= fruit[1] - EATING_RADIUS && snake[0][1] <= fruit[1] + EATING_RADIUS))
-    {
-
-      snakeLength += 10;
-      fruit[0] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
-      fruit[1] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
-    }
-
-
-    if (snake[0][1] == BOARD_SIZE)
-    {
-      snake[0][1] = 0;
-    }
-
-  }
-
-  else if(directionState == DOWN)
-  {
-    snake[0][0] = snake[1][0];
-    snake[0][1] = snake[1][1]-1;
-
-    if(((snake[0][0] >= fruit[0] - EATING_RADIUS && snake[0][0] <= fruit[0] + EATING_RADIUS)) && (snake[0][1] >= fruit[1] - EATING_RADIUS && snake[0][1] <= fruit[1] + EATING_RADIUS))
-    {
-      snakeLength += 10;
-      fruit[0] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
-      fruit[1] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
-    }
-
-
-    if (snake[0][1] == 0)
-    {
-      snake[0][1] = BOARD_SIZE;
-    }
-
-  }
-
-  else if(directionState == RIGHT)
-  {
-    snake[0][0] = snake[1][0]+1;
-    snake[0][1] = snake[1][1];
-
-
-    if(((snake[0][0] >= fruit[0] - EATING_RADIUS && snake[0][0] <= fruit[0] + EATING_RADIUS)) && (snake[0][1] >= fruit[1] - EATING_RADIUS && snake[0][1] <= fruit[1] + EATING_RADIUS))
-    {
-      snakeLength += 10;
-      fruit[0] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
-      fruit[1] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
-    }
-
-    if (snake[0][0] == BOARD_SIZE)
-    {
-      snake[0][0] = 0;
-    }
-  }
-
-  else if(directionState == LEFT)
-  {
-    snake[0][0] = snake[1][0]-1;
-    snake[0][1] = snake[1][1];
-
-    
-    if(((snake[0][0] >= fruit[0] - EATING_RADIUS && snake[0][0] <= fruit[0] + EATING_RADIUS)) && (snake[0][1] >= fruit[1] - EATING_RADIUS && snake[0][1] <= fruit[1] + EATING_RADIUS))
-    {
-      snakeLength += 10;
-      fruit[0] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
-      fruit[1] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
-    }
-
-    if (snake[0][0] == 0)
-    {
-      snake[0][0] = BOARD_SIZE;
-    }
-
-  }
-}
-
 // DRAWING BOARD
 void drawVerticalLine(int x, int y_start, int y_end)
 {
@@ -207,6 +107,148 @@ void drawHorizontalLine(int y, int x_start, int x_end)
   }
 }
 
+void drawSnake()
+{
+  for (int i = 0; i < snakeLength; i++)
+  {
+    drawPoint(snake[i][0], snake[i][1]);
+  }
+
+}
+
+bool checkSelfCollision() {
+  for (int i = 1; i < snakeLength; i++) { // Start at 1 to exclude the head
+    if (snake[0][0] == snake[i][0] && snake[0][1] == snake[i][1]) {
+      return true; // Collision detected
+    }
+  }
+  return false;
+}
+
+void drawO(int startX, int startY) {
+  drawHorizontalLine(startY, startX, startX + 25);
+  drawVerticalLine(startX + 25, startY, startY + 25);
+  drawHorizontalLine(startY + 25, startX + 25, startX);
+  drawVerticalLine(startX, startY + 25, startY);
+
+}
+
+void drawV(int startX, int startY, int size) {
+  for (int i = 0; i <= size; i++) {
+    drawPoint(startX + i, startY + i);        // Diagonal down-right
+    drawPoint(startX + size - i, startY + i); // Diagonal down-left
+  }
+}
+
+void drawE(int startX, int startY, int size) {
+  for (int i = 0; i <= size; i++) {
+    drawPoint(startX, startY + i);           // Vertical line
+    if (i == 0 || i == size / 2 || i == size) {
+      for (int j = 0; j <= size / 2; j++) {
+        drawPoint(startX + j, startY + i);   // Horizontal lines
+      }
+    }
+  }
+}
+
+void drawR(int startX, int startY, int size) {
+  for (int i = 0; i <= size; i++) {
+    drawPoint(startX, startY + i);           // Vertical line
+    if (i == 0 || i == size / 2) {
+      for (int j = 0; j <= size / 2; j++) {
+        drawPoint(startX + j, startY + i);   // Horizontal lines
+      }
+    }
+    if (i > size / 2) {
+      drawPoint(startX + (i - size / 2), startY + i); // Diagonal leg
+    }
+  }
+}
+
+void drawGameOver() {
+  int startX = 50;  // Starting X position
+  int startY = 100; // Starting Y position
+  int size = 20;    // Size of each letter
+
+  drawO(startX, startY);
+  drawV(startX + size + 10, startY, size);  // Spacing between letters
+  drawE(startX + 2 * (size + 10), startY, size);
+  drawR(startX + 3 * (size + 10), startY, size);
+}
+
+void nextSnake()
+{
+  // Shift all elements back by one
+  for (int i = snakeLength; i > 0; i--)
+  {
+      snake[i][0] = snake[i - 1][0];
+      snake[i][1] = snake[i - 1][1];
+  }
+
+
+  if(directionState == UP)
+  {
+    snake[0][0] = snake[1][0];
+    snake[0][1] = snake[1][1]+1;
+
+    if (snake[0][1] == BOARD_SIZE)
+    {
+      snake[0][1] = 0;
+    }
+
+  }
+
+  else if(directionState == DOWN)
+  {
+    snake[0][0] = snake[1][0];
+    snake[0][1] = snake[1][1]-1;
+
+    if (snake[0][1] == 0)
+    {
+      snake[0][1] = BOARD_SIZE;
+    }
+
+  }
+
+  else if(directionState == RIGHT)
+  {
+    snake[0][0] = snake[1][0]+1;
+    snake[0][1] = snake[1][1];
+
+    if (snake[0][0] == BOARD_SIZE)
+    {
+      snake[0][0] = 0;
+    }
+  }
+
+  else if(directionState == LEFT)
+  {
+    snake[0][0] = snake[1][0]-1;
+    snake[0][1] = snake[1][1];
+
+    if (snake[0][0] == 0)
+    {
+      snake[0][0] = BOARD_SIZE;
+    }
+  }
+
+  if(checkSelfCollision())
+  {
+    while(true)
+    {
+      drawGameOver();
+      // Halt game execution
+    }
+  }
+
+  if(((snake[0][0] >= fruit[0] - EATING_RADIUS && snake[0][0] <= fruit[0] + EATING_RADIUS)) && (snake[0][1] >= fruit[1] - EATING_RADIUS && snake[0][1] <= fruit[1] + EATING_RADIUS))
+  {
+    snakeLength += 20;
+    fruit[0] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
+    fruit[1] = random(EATING_RADIUS, BOARD_SIZE - EATING_RADIUS);
+  }
+}
+
 // Function to draw the boundary square
 // Function to draw the boundary square
 void drawSquare() {
@@ -215,7 +257,6 @@ void drawSquare() {
   drawVerticalLine(BOARD_SIZE, 0, BOARD_SIZE);  // Right side
   drawHorizontalLine(0, 0, BOARD_SIZE); // Bot side
 }
-
 
 void setup() {
   // To create random numbers
